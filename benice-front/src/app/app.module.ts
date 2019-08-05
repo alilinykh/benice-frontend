8d37eabd923dda5b404d3d49bc52aa9db7cbbbc1
alilinykh <aililnykh@dtcc.edu>
2019-08-05 16:46:46 -0400

    add page404/new contact form

--- benice-front/src/app/app.module.ts
+++ benice-front/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { ContactRouteActivator } from './contact-route-activator';
+import { ToastrService } from './sevices/toastr.service';
+import { ContactServiceService } from './sevices/contact-service.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,18 +8,28 @@ import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { Page404Component } from './errors/page404.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
     ContactDetailsComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContactPageComponent,
+    NewContactComponent,
+    Page404Component
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
-  providers: [],
+  providers: [ContactServiceService, ToastrService, ContactRouteActivator],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- benice-front/src/app/contact-list/contact-list.component.ts
+++ benice-front/src/app/contact-list/contact-list.component.ts
@@ -1,53 +1,28 @@
+import { ToastrService } from './../sevices/toastr.service';
+import { ContactServiceService } from './../sevices/contact-service.service';
 import { Component, OnInit } from '@angular/core';
 
+// declare let toastr: any;
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
+
 export class ContactListComponent implements OnInit {
+  contacts: any[];
 
-  constructor() { }
-
-  contacts = [ {
-    id: 1,
-    name: 'Alex',
-    category: 'Important',
-    description: 'some random info',
-    picUrl: 'null',
-    location: 'Zipcode'
-  },
-  {
-    id: 2,
-    name: 'Bob',
-    category: 'work',
-    description: ' frontend magician',
-    picUrl: 'www.google.com',
-    location: 'Zipcode'
-  },
-  {
-    id: 2,
-    name: 'Ben',
-    category: 'work',
-    description: ' sdfdsfsf',
-    picUrl: 'www.google.com',
-    location: 'Zipcode'
-  },
-  {
-    id: 2,
-    name: 'Ed',
-
-    description: ' asdf',
-    picUrl: 'www.google.com',
-    location: 'Zipcode'
+  constructor(private conctactService: ContactServiceService, private toastr: ToastrService) {
   }
-];
-
   handleEventClicked(data: any) {
     console.log('recieved: ', data);
   }
 
   ngOnInit() {
+    this.contacts = this.conctactService.getContacts();
+  }
+  handleContactClickToastr(contactName: any) {
+    this.toastr.success(contactName);
   }
 
 }
